c01b6da2c106630e9596375dd4d46889b7ec33e7
97mohatah <131670439+97mohatah@users.noreply.github.com>
2023-05-10 00:26:28 +0430

    Update mohamad_Taher_Taherpoor_E09.py

--- mohamad_Taher_Taherpoor_E09.py
+++ mohamad_Taher_Taherpoor_E09.py
@@ -4,18 +4,20 @@ gdb_path = r"{}".format(input("Type your address: "))
 arcpy.env.workspace =gdb_path
 
 featureList=arcpy.ListFeatureClasses()
-count_Point=0
-count_Line=0
-count_Polygon=0
+points=[]
+lines=[]
+polygons=[]
 
 for item in featureList:
     Des=arcpy.Describe(item)
     if Des.shapetype =="Point":
-        count_Point +=1
+        points.append(item)
     elif Des.shapetype =="Polyline":
-        count_Line +=1
+        lines.append(item)
     elif Des.shapetype =="Polygon":
-        count_Polygon +=1    
-    print("Layer name is : ",Des.name," ;","Layer type is : ",Des.shapetype)
-print("""in this GDB, there are:\n{0} Point layers. \n{1} Line layers. 
-{2} Polygon layers.""".format(count_Point,count_Line,count_Polygon))
+        polygons.append(item)    
+print("""in this GDB, there are:
+{0} Point layers: {1}
+{2} Line layers: {3} 
+{4} Polygon layers: {5}""".format(len(points)," ; ".join(points)
+,len(lines)," ; ".join(lines),len(polygons)," ; ".join(polygons)))
